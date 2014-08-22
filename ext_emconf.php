d61633c450c719c2e9eb87727a1b22276f8ce5d9
uoziod <sv@rv7.ru>
2014-08-22 10:24:28 +0400

    TYPO3 version dependency fix

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "shop_manager".
 #
-# Auto generated 17-07-2014 19:49
+# Auto generated 22-08-2014 06:21
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'Shop System (tt_products) management system. This is backend module with 2 sections. With "Catalog Items" you can manage shop categories and items pretty simple. "Categories Manager" is a useful order status manager with customers e-mail notification support.',
 	'category' => 'module',
 	'shy' => 0,
-	'version' => '1.0.3',
+	'version' => '1.0.4',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
@@ -34,7 +34,8 @@ $EM_CONF[$_EXTKEY] = array(
 	'CGLcompliance_note' => '',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '4.1.0-6.2.99',
+			'php' => '5.2.0-0.0.0',
+			'typo3' => '4.5.17-6.2.99',
 		),
 		'conflicts' => array(
 		),
@@ -42,6 +43,8 @@ $EM_CONF[$_EXTKEY] = array(
 		),
 	),
 	'_md5_values_when_last_written' => 'a:14:{s:21:"ext_conf_template.txt";s:4:"dbfc";s:12:"ext_icon.gif";s:4:"a39a";s:14:"ext_tables.php";s:4:"6598";s:14:"doc/manual.sxw";s:4:"cfee";s:13:"mod1/conf.php";s:4:"18a3";s:14:"mod1/footer.js";s:4:"cc95";s:14:"mod1/header.js";s:4:"403d";s:14:"mod1/index.php";s:4:"4698";s:14:"mod1/jquery.js";s:4:"73a9";s:18:"mod1/locallang.xml";s:4:"9ad7";s:22:"mod1/locallang_mod.xml";s:4:"d353";s:22:"mod1/mod_template.html";s:4:"6122";s:19:"mod1/moduleicon.gif";s:4:"a39a";s:15:"mod1/styles.css";s:4:"0adf";}',
+	'suggests' => array(
+	),
 );
 
 ?>
